3d886bd8518c515d96ba6e21b989cad8a50d192f
vibhcool <vibhcool@gmail.com>
2017-09-17 22:09:25 +0530

    Add price/cpu attribute

--- Model/Server.py
+++ Model/Server.py
@@ -3,6 +3,7 @@ class Server():
 
     cpu_count = 0
     price = 0
+    price_per_cpu = 0
     data_center = ''
     server_type = ''
 
@@ -12,6 +13,12 @@ class Server():
         self.data_center = data_center
         self.price_per_cpu = (self.price/self.cpu_count)
         self.server_type = server_type
+
+    def price_per_cpu(self):
+        return self.price_per_cpu
+
+    def set_cpu_count(self, cpu_count):
+        self.cpu_count = cpu_count
 
     def get_cpu_count(self):
         return self.cpu_count
