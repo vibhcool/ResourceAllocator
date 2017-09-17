4da954a19f30a99d5de76b6d0b6e765374b2d0a0
vibhcool <vibhcool@gmail.com>
2017-09-17 21:18:53 +0530

    Add Server object

---
+++ Model/Server.py
@@ -0,0 +1,29 @@
+
+class Server():
+
+    cpu_count = 0
+    price = 0
+    data_center = ''
+    server_type = ''
+
+    def __init__(self, cpu_count, price, data_center, hours, server_type):
+        self.cpu_count = cpu_count
+        self.price = price * hours
+        self.data_center = data_center
+        self.price_per_cpu = (self.price/self.cpu_count)
+        self.server_type = server_type
+
+    def get_cpu_count(self):
+        return self.cpu_count
+
+    def get_price(self):
+        return self.cpu_count
+
+    def set_price(self, price):
+        self.price = price
+
+    def set_data_center(self, data_center):
+        self.data_center = data_center
+
+    def get_data_center(self):
+        return self.cpu_count
--- main.py
+++ main.py
@@ -1,1 +1,53 @@
-print('Hello')
+from Model.Server import Server
+
+type_server = {
+    'large': 1,
+    'xlarge': 2,
+    '2xlarge': 4,
+    '4xlarge': 8,
+    '8xlarge': 16,
+    '10xlarge': 32,
+}
+
+k = {
+        "us-east": {
+            "large": 0.12,
+            "xlarge": 0.23,
+            "2xlarge": 0.45,
+            "4xlarge": 0.774,
+            "8xlarge": 1.4,
+            "10xlarge": 2.82
+        },
+        "us-west": {
+            "large": 0.14,
+            "2xlarge": 0.413,
+            "4xlarge": 0.89,
+            "8xlarge": 1.3,
+            "10xlarge": 2.97
+        },
+        "us-north": {
+            "large": 0.04,
+            "2xlarge": 0.113,
+            "4xlarge": 0.99,
+            "8xlarge": 0.3,
+            "10xlarge": 2.07
+        },
+    }
+
+
+
+def get_server_list(instances, hours):
+    server_list = []
+    for data_center in instances.keys():
+        for server in instances[data_center].keys():
+            a = Server(type_server[server], instances[data_center][server], data_center, hours, server)
+            server_list.append(a)
+    return server_list
+
+def get_costs(instances, hours, cpus=-1, money=-1.0):
+
+    server_list = get_server_list(instances, hours)
+    result = []
+    return result
+
+print(get_costs(k, 10, 20, 23.02))
